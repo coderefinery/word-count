1a54d0cd9150ef0b669a299a6a3c77ca27c92585
Samantha Wittke <32324155+samumantha@users.noreply.github.com>
2024-03-17 08:53:58 +0200

    update code location

--- run_all_loop.sh
+++ run_all_loop.sh
@@ -3,6 +3,6 @@
 # loop over all books to count words and plot the frequency of the 10 most common words per book
 
 for title in abyss isles last sierra; do
-    python statistics/count.py data/${title}.txt > statistics/${title}.data
-    python plot/plot.py --data-file statistics/${title}.data --plot-file plot/${title}.png
+    python code/count.py data/${title}.txt > statistics/${title}.data
+    python code/plot.py --data-file statistics/${title}.data --plot-file plot/${title}.png
 done
